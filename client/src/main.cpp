6b3dc283e13ad456a613e8979e08c7ecb3abfa22
hackpulsar <kobylnyk.vladyslav@gmail.com>
2024-11-14 13:41:24 +0100

    windows docking and small elements' resize fix

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -71,11 +71,24 @@ int main(int, char**)
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.FontGlobalScale = 1.25f;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    io.FontGlobalScale = 1.0f;
+
+    // TODO: search for a font in the system, not project
+    io.FontDefault = io.Fonts->AddFontFromFileTTF("../../imgui/misc/fonts/Roboto-Medium.ttf", 18.0f);
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
     //ImGui::StyleColorsLight();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -97,6 +110,7 @@ int main(int, char**)
     std::string message;
 
     ImVector<ImVector<ImVec2>> lines;
+    ImVec4 color = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
 
     Core::Networking::TCPClient client;
     client.msgRecCallback = [&chat] (const std::string& message) { chat.push_back(message); };
@@ -162,17 +176,48 @@ int main(int, char**)
             ImGui::End();
         }
         else {
-            ImGui::Begin("Chat", nullptr, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
+            // Dockspace
+            static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None; // Config flags for the Dockspace
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
 
-            ImGui::BeginChild("Scrolling", ImVec2(0, 400));
-            for (const auto& m : chat) {
-                ImGui::Text("%s", m.c_str());
+            // If so, get the main viewport:
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+
+            // Set the parent window's position, size, and viewport to match that of the main viewport. This is so the parent window
+            // completely covers the main viewport, giving it a "full-screen" feel.
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+
+            // Set the parent window's styles to match that of the main viewport:
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f); // No corner rounding on the window
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f); // No border around the window
+
+            // Manipulate the window flags to make it inaccessible to the user (no titlebar, resize/move, or navigation)
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+            // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+            // and handle the pass-thru hole, so the parent window should not have its own background:
+            if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+                window_flags |= ImGuiWindowFlags_NoBackground;
+
+            ImGui::Begin("DockSpace", nullptr, window_flags);
+
+            // Pop the two style rules set in Fullscreen mode - the corner rounding and the border size.
+            ImGui::PopStyleVar(2);
+
+            if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+            {
+                ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
+                ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
             }
-            ImGui::EndChild();
+
+            ImGui::Begin("Chat", nullptr, ImGuiWindowFlags_NoScrollbar);
 
             ImGui::InputText(" ", &message);
             ImGui::SameLine();
-            if (ImGui::Button("Send")) {
+            if (ImGui::Button("Send", { ImGui::GetContentRegionAvail().x, 0.f }) && !message.empty()) {
                 using namespace Core::Networking;
                 using namespace Core::Networking::Package;
 
@@ -184,11 +229,17 @@ int main(int, char**)
                 message = "";
             }
 
-            ImGui::End();
+            ImGui::BeginChild("Scrolling");
+            for (const auto& m : chat) {
+                ImGui::Text("%s", m.c_str());
+            }
+            ImGui::EndChild();
+
+            ImGui::End(); // Chat
 
             ImGui::Begin("Board", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
 
-            ImGui::BeginChild("Canvas", ImVec2(800, 600));
+            ImGui::BeginChild("Canvas", ImGui::GetContentRegionAvail());
 
             static ImVec2 scrolling(0.0f, 0.0f);
             static bool enableGrid = true;
@@ -271,10 +322,6 @@ int main(int, char**)
             // Draw lines
             for (const auto& line : lines) {
                 for (int i = 0; i < line.size() - 1; i++) {
-                    /*LOG_LINE("Line " << i);
-                    for (const auto& p : line)
-                        LOG_LINE(p.x << " " << p.y); */
-
                     draw_list->AddLine(
                         ImVec2(origin.x + line[i].x, origin.y + line[i].y),
                         ImVec2(origin.x + line[i + 1].x, origin.y + line[i + 1].y),
@@ -287,6 +334,8 @@ int main(int, char**)
 
             ImGui::EndChild();
             ImGui::End();
+
+            ImGui::End(); // Dockspace
         }
 
         // Rendering
@@ -294,14 +343,18 @@ int main(int, char**)
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glClearColor(
-            clear_color.x * clear_color.w,
-            clear_color.y * clear_color.w,
-            clear_color.z * clear_color.w,
-            clear_color.w
-        );
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Update and Render additional Platform Windows
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
         glfwSwapBuffers(window);
     }
